56fe97aad9b3845991681e81376e6db529be9f80
yatishgautam00 <yatishgoutam00@gmail.com>
2024-08-10 19:56:24 +0530

    ui update of doctor list

--- app/components/DoctorList.js
+++ app/components/DoctorList.js
@@ -1,57 +1,56 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import { firestore } from '@/lib/firebase';
-import { collection, getDocs } from 'firebase/firestore';
+"use client";
+import React, { useEffect, useState } from "react";
+import { firestore } from "@/lib/firebase";
+import { collection, getDocs } from "firebase/firestore";
 import { Skeleton } from "@/components/ui/skeleton";
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 import { usePathname } from "next/navigation";
 
-function DoctorList({ heading = 'Popular Doctor', }) {
+function DoctorList({ heading = "Popular Doctor" }) {
   const [doctorList, setDoctorList] = useState([]);
   const [doctor, setDoctor] = useState();
 
   const params = usePathname();
-    const category01 = params.split("/")[2];
-    const categorycond = params.split("/")[1];
-    let paramsCondition = false;
+  const category01 = params.split("/")[2];
+  const categorycond = params.split("/")[1];
+  let paramsCondition = false;
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const querySnapshot = await getDocs(collection(firestore, 'users'));
-        const doctorList = querySnapshot.docs.map(doc => doc.data());
-        const doctors = querySnapshot.docs.map(doc => ({
+        const querySnapshot = await getDocs(collection(firestore, "users"));
+        const doctorList = querySnapshot.docs.map((doc) => doc.data());
+        const doctors = querySnapshot.docs.map((doc) => ({
           id: doc.id,
-          ...doc.data()
+          ...doc.data(),
         }));
         setDoctorList(doctors);
-        
       } catch (error) {
         console.error("Error fetching categories: ", error);
       }
     };
     fetchCategories();
   }, []);
-// console.log(doctorList)
+  // console.log(doctorList)
   const renderDoctors = () => {
     return doctorList.length > 0
       ? doctorList.map((doctor, index) => {
-          if (categorycond === 'search') {
+          if (categorycond === "search") {
             paramsCondition = true;
-            return doctor.role === 'doctor' && doctor.category === category01 ? (
-              
+            return doctor.role === "doctor" &&
+              doctor.category === category01 ? (
               <div
                 key={index}
-                className="border-[1px] rounded-lg p-3 bg-gray-25 bg-gray-50 cursor-pointer hover:border-primary hover:shadow-sm transition-all ease-in-out"
+                className="border-[1px] rounded-lg p-3 hover:scale-105 bg-gray-25 bg-gray-50 cursor-pointer hover:border-primary hover:shadow-sm transition-all ease-in-out"
               >
                 <Image
                   src={doctor.doctorImg}
                   alt="vaidyapadma doctor img"
                   width={500}
                   height={250}
-                  className="h-[200px] w-full object-cover rounded-lg"
+                  className="h-60 w-full object-cover rounded-lg"
                 />
                 <div className="mt-3 items-baseline flex flex-col gap-1">
                   <h2 className="text-[10px] bg-blue-100 p-1 rounded-full px-2 text-primary">
@@ -60,7 +59,7 @@ function DoctorList({ heading = 'Popular Doctor', }) {
                   <h2 className="font-bold">{doctor.name}</h2>
                   <h2 className="text-primary text-sm">{doctor.exp} years</h2>
                   <h2 className="text-gray-500 text-sm">{doctor.address}</h2>
-                  <Link href={'/details/'+doctor.id} className="w-full">
+                  <Link href={"/details/" + doctor.id} className="w-full">
                     <h2 className="p-2 px-3 border-[1px] border-primary text-primary rounded-full w-full text-center text-[11px] mt-2 cursor-pointer hover:bg-primary hover:text-white">
                       Book Now
                     </h2>
@@ -68,18 +67,19 @@ function DoctorList({ heading = 'Popular Doctor', }) {
                 </div>
               </div>
             ) : null;
-          } if( !paramsCondition ) {
-            return doctor.role === 'doctor' ? (
+          }
+          if (!paramsCondition) {
+            return doctor.role === "doctor" ? (
               <div
                 key={index}
-                className="border-[1px] rounded-lg p-3 bg-gray-25 bg-gray-50 cursor-pointer hover:border-primary hover:shadow-sm transition-all ease-in-out"
+                className="border-[1px] shadow-md  hover:scale-105 rounded-lg p-2 md:p-3 bg-gray-25 bg-gray-50 cursor-pointer hover:border-primary hover:shadow-sm transition-all ease-in-out"
               >
                 <Image
                   src={doctor.doctorImg}
-                  alt="Vaidyapadma doctor"
+                  alt="vaidyapadma doctor img"
                   width={500}
                   height={250}
-                  className="h-[200px] w-full object-cover rounded-lg"
+                  className="h-64  object-cover w-full rounded-lg "
                 />
                 <div className="mt-3 items-baseline flex flex-col gap-1">
                   <h2 className="text-[10px] bg-blue-100 p-1 rounded-full px-2 text-primary">
@@ -88,7 +88,7 @@ function DoctorList({ heading = 'Popular Doctor', }) {
                   <h2 className="font-bold">{doctor.name}</h2>
                   <h2 className="text-primary text-sm">{doctor.exp} years</h2>
                   <h2 className="text-gray-500 text-sm">{doctor.address}</h2>
-                  <Link href={'/details/'+doctor.id} className="w-full">
+                  <Link href={"/details/" + doctor.id} className="w-full">
                     <h2 className="p-2 px-3 border-[1px] border-primary text-primary rounded-full w-full text-center text-[11px] mt-2 cursor-pointer hover:bg-primary hover:text-white">
                       Book Now
                     </h2>
@@ -98,8 +98,7 @@ function DoctorList({ heading = 'Popular Doctor', }) {
             ) : null;
           }
         })
-      : 
-      [1, ].map((item, index) => (
+      : [1].map((item, index) => (
           <div
             className="grid grid-cols-2 sm:grid-cols-2 gap-10 align-baseline justify-content-center md:grid-cols-3 lg:grid-cols-3 mt-5 items-center"
             key={index}
@@ -112,9 +111,9 @@ function DoctorList({ heading = 'Popular Doctor', }) {
   };
 
   return (
-    <div className="mb-10 px-10">
+    <div className="mb-10 px-4 md:px-10">
       <h2 className="font-bold text-xl">{heading}</h2>
-      <div className="grid grid-cols-2 gap-7 md:grid-cols-2 lg:grid-cols-4 mt-5">
+      <div className="grid grid-cols-2 gap-4 md:gap-7 md:grid-cols-2 lg:grid-cols-4 mt-5">
         {renderDoctors()}
       </div>
     </div>
--- app/page.js
+++ app/page.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import Hero from './components/Hero'
-import CategoryList from './components/CategoryList'
-import SearchCategory from './components/SearchCategory'
-import DoctorList from './components/DoctorList'
+import React from "react";
+import Hero from "./components/Hero";
+import CategoryList from "./components/CategoryList";
+import SearchCategory from "./components/SearchCategory";
+import DoctorList from "./components/DoctorList";
 
 function page() {
   return (
-    <div className=''>
+    <div className="">
       <Hero />
-      
-      <SearchCategory />
 
-     <DoctorList />
+      <SearchCategory />
+      <div className="md:px-14 px-0">
+        <DoctorList />
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
